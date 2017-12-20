6e13d9b39c1c7ba6fde0cbab2cc1137f0cd4fb8f
Andrews Medina <andrewsmedina@gmail.com>
2017-12-20 01:30:19 -0300

    get updates

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const token = ""
+
+func getUpdates() ([]byte, error) {
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/getUpdates", token)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return data, nil
+}
+
+func main() {
+	updates, err := getUpdates()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(updates))
+}
