6f725d6b4b5053293c493cb9d6d9478bf4a92344
Andrews Medina <andrewsmedina@gmail.com>
2017-12-20 22:08:31 -0300

    send mensage on startup

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 )
 
-var token = os.Getenv("TOKEN")
+var (
+	token  = os.Getenv("TOKEN")
+	chatID = os.Getenv("CHAT_ID")
+)
 
 func getUpdates() ([]byte, error) {
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/getUpdates", token)
@@ -26,7 +31,19 @@ func getUpdates() ([]byte, error) {
 	return data, nil
 }
 
+func sendMessage(chatID, text string) error {
+	u := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", token)
+	contentType := "application/x-www-form-urlencoded"
+	values := url.Values{}
+	values.Add("chat_id", chatID)
+	values.Add("text", text)
+	reader := strings.NewReader(values.Encode())
+	_, err := http.Post(u, contentType, reader)
+	return err
+}
+
 func main() {
+	sendMessage(chatID, "up")
 	updates, err := getUpdates()
 	if err != nil {
 		log.Fatal(err)
